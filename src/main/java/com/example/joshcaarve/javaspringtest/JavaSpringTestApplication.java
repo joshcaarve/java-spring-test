ecfad30dcf55327000e73c67f5a5415cae2970f3
jbrard <Joshuaeli1234@gmail.com>
2021-09-07 18:59:53 -0400

    Added: init

---
+++ src/main/java/com/example/joshcaarve/javaspringtest/JavaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.joshcaarve.javaspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTestApplication.class, args);
+	}
+
+}
